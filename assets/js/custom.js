3bc369cf9740435e4bf56318191994e93d8e10ae
Mahad Amir <mamir3012@gmail.com>
2021-01-12 11:03:56 +0500

    Integrate template index page

---
+++ assets/js/custom.js
@@ -0,0 +1,18 @@
+$.i18n.debug = true;
+jQuery(function($) {
+  $.i18n({
+    locale: 'de'
+  }).load( {
+    'en': './assets/js/i18n/en.json',
+    'de': './assets/js/i18n/de.json'
+  } ).done(function() {
+    $('html').i18n();
+
+    $('.switch-locale').on('click', 'a', function(e) {
+        console.log('here')
+        e.preventDefault();
+        $.i18n().locale = $(this).data('locale');
+        $('html').i18n();
+      });
+    });
+});
